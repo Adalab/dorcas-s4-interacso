f20be65ae89904990abae9353e9c90c5e3a1ae71
andre <andreina.vargas@gmail.com>
2018-09-20 22:30:00 +0200

    fix days

--- src/App.js
+++ src/App.js
@@ -268,8 +268,8 @@ getCalendarDates() {
     this.setState({
       weekChartData: teamData,
       memberPics: memberPicsData,
-      averageTask: (averageTask/json.data.length) / 7,
-      averageCommits: (averageCommits/json.data.length) / 7
+      averageTask: (averageTask/json.data.length) / 5,
+      averageCommits: (averageCommits/json.data.length) / 5
     })
   }
 
